de3e699bb320c6d98f15da51b5cfaae0bbbc3f8c
zg2323 Gao <zg2323@columbia.edu>
2021-10-10 23:11:21 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,23 +8,28 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
-    if params.has_key?(:ratings)
-      @ratings_to_show = params[:ratings].keys
-      @ratings_to_show_hash = Hash[@ratings_to_show.collect{|key| [key, '1']}]
-    else
-      @ratings_to_show = []
-    end
+
+    # redirect when params has no key but session has key
+    if (!params.has_key?(:ratings) && session.has_key?(:ratings))
+      redirect_to movies_path(:ratings => Hash[session[:ratings].collect{|key| [key, '1']}], :sort_by => params[:sort_by]) and return 
+    elsif (!params.has_key?(:sort_by) && session.has_key?(:sort_by))
+      redirect_to movies_path(:ratings => params[:ratings], :sort_by => session[:sort_by]) and return 
+    end 
     
+    if !params.has_key?(:ratings)
+      @ratings_to_show = @all_ratings
+    else 
+      @ratings_to_show = params[:ratings].keys
+    end
+    @ratings_to_show_hash = Hash[@ratings_to_show.collect{|key| [key, '1']}]
+    session[:ratings] = @ratings_to_show
     @movies = Movie.with_ratings(@ratings_to_show)
     
     @sort_by = params[:sort_by]
-    if @sort_by == "title"
-      @movies = @movies.order(params[:sort_by])
-      @title_class = "hilite bg-warning"
-    elsif @sort_by == "release_date"
-      @movies = @movies.order(params[:sort_by])
-      @release_date_class = "hilite bg-warning"
-    end
+    @movies = @movies.order(@sort_by) if @sort_by != ''
+    session[:sort_by] = @sort_by
+    @title_class = "hilite bg-warning" if @sort_by == "title"
+    @release_date_class = "hilite bg-warning" if @sort_by == "release_date"   
       
   end
 
